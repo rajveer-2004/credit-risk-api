d1a11ee08946448166ea35c830f251c9c5820212
Rajveer Singh <rvs51101@gmail.com>
2026-06-30 14:24:50 +0530

    Fix CORS middleware configuration

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = FastAPI(title="Credit Risk API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
